02dff455e4435419da7b6e6bc03bf6ea7a99a613
huydev0401 <quanghuynguyennqh@gmail.com>
2023-11-08 22:03:34 +0700

    Add background for button hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
     <section className="w-full h-[50vh] md:h-[80vh] lg:h-screen bg-[url('/images/pbc30-hero-mb.png')] lg:bg-[url('/images/pbc30-hero.png')] bg-center bg-no-repeat bg-cover">
       <div className="flex items-end justify-center h-full">
         <button
-          className="!hidden md:!flex button mb-[210px] lg:mb-[140px]"
+          className="!hidden md:!flex button mb-[210px] lg:mb-[140px] !bg-[#000442]/40"
           onClick={() => setShowRegister(true)}
         >
           <span>ĐĂNG KÝ NGAY</span>
